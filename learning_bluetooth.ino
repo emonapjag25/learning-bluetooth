6fc4e6b63a669074b5be5d8116063cc9e4c19f80
Emona Jagaselvan <emonapjag25@emonas-air.milton.edu>
2025-02-14 13:53:47 -0500

    added davis character

--- learning_bluetooth.ino
+++ learning_bluetooth.ino
@@ -5,13 +5,23 @@ int const TX_PIN = 0; //this is the TX pin, the transmits
 SoftwareSerial tooth(TX_PIN, RX_PIN); //makes a bluetooth object
 //set tx and rx pins
 //tx goes first, then rx
-
+char davis;
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  tooth.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (tooth.available()>0){
+      davis = tooth.read();
+      tooth.print("reading new input: ");
+      tooth.println(davis);
+  }
 
+  if(davis == 'd'){
+    Serial.print("davis has value!");
+  }
+  delay(100);
 }
